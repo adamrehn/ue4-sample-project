67175dff383b2c93c3ad352274cdd964f8176827
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-04-26 13:30:55 +1000

    Initial source commit

---
+++ Source/SampleProject/SampleProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SampleProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASampleProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASampleProjectGameMode();
+};
+
+
+
